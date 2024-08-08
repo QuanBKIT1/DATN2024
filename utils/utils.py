9a9d9ab841ac4ce358584bf329b3b068f89e7248
Duc Quan <leducquan16112002@gmail.com>
2024-08-08 22:07:11 +0700

    update

--- utils/utils.py
+++ utils/utils.py
@@ -1,20 +1,61 @@
 import onnxruntime as ort
 import numpy as np
+import cv2
 
+# Define keypoint selected
 joints = {
     27: np.concatenate(([0, 1, 2, 5, 6, 7, 8],    # upper body keypoints
                         [91, 95, 96, 99, 100, 103, 104, 107, 108, 111], [112, 116, 117, 120, 121, 124, 125, 128, 129, 132]), axis=0),
-
     31: np.concatenate(([0, 1, 2, 5, 6, 7, 8],    # upper body keypoints
                         [71, 74, 77, 80],  # mouth keypoints
                         [91, 95, 96, 99, 100, 103, 104, 107, 108, 111], [112, 116, 117, 120, 121, 124, 125, 128, 129, 132]), axis=0),
-
     49: np.concatenate(([0, 1, 2, 5, 6, 7, 8],     # upper body keypoints
                         np.arange(91, 133)   # hand keypoints
                         ), axis=0),
 
 
 }
+
+# Define edge connections
+edges = {}
+
+edges[27] = [(1, 2), (1, 3), (1, 6), (1, 7), (6, 8), (7, 9),  # upper body connection
+             (8, 92), (92, 96), (92, 97), (92, 101), (92, 105),
+             (92, 109), (97, 100), (101, 104), (105,
+                                                108), (109, 112),  # Right hand connection
+             (9, 113), (113, 117), (113, 118), (113, 122), (113, 126),
+             (113, 130), (118, 121), (122, 125), (126,
+                                                  129), (130, 133)  # Left hand connection
+             ]
+
+
+edges[31] = [(1, 2), (1, 3), (1, 6), (1, 7), (6, 8), (7, 9),  # upper body connection
+             (72, 75), (72, 81), (75, 78), (78, 81),  # Mouth connection
+             (8, 92), (92, 96), (92, 97), (92, 101), (92, 105),
+             (92, 109), (97, 100), (101, 104), (105,
+                                                108), (109, 112),  # Right hand connection
+             (9, 113), (113, 117), (113, 118), (113, 122), (113, 126),
+             (113, 130), (118, 121), (122, 125), (126,
+                                                  129), (130, 133)  # Left hand connection
+             ]
+
+edges[49] = [(1, 2), (1, 3), (1, 6), (1, 7), (6, 8), (7, 9),  # upper body connection
+             (8, 92), (92, 93), (92, 97), (92,
+                                           101), (92, 105), (92, 109),
+             (93, 94), (94, 95), (95, 96), (97, 98), (98, 99), (99, 100),
+             (101, 102), (102, 103), (103, 104), (105,
+                                                  106), (106, 107), (107, 108),
+             # Right hand connection
+             (109, 110), (110, 111), (111, 112),
+             (9, 113), (113, 114), (113, 118), (113,
+                                                122), (113, 126), (113, 130),
+             (114, 115), (115, 116), (116, 117), (118,
+                                                  119), (119, 120), (120, 121),
+             (122, 123), (123, 124), (124, 125), (126,
+                                                  127), (127, 128), (128, 129),
+             (130, 131), (131, 132), (132, 133)
+             # Left hand connection
+             ]
 
 
 def prepare_data(results, num_keypoint=27):
@@ -45,7 +86,6 @@ def prepare_data(results, num_keypoint=27):
     # Shape x: (150,num_keypoint,3) -> Reshape to (1,3,150,num_keypoint,1)
     data = data.transpose((2, 0, 1))
     data = np.expand_dims(data, axis=-1)
-
     #   Normalize data
     data = normalize(data)
     return data
@@ -76,22 +116,42 @@ def build_session(onnx_file: str, device: str = 'cpu') -> ort.InferenceSession:
     return sess
 
 
-flip_index = {
-    'keypoint-27': np.array([0, 2, 1, 4, 3, 6, 5, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]),
-    'keypoint-31': np.array([0, 2, 1, 4, 3, 6, 5, 9, 8, 7, 10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]),
-    'keypoint-49': np.array([0, 2, 1, 4, 3, 6, 5, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
-                             44, 45, 46, 47, 48, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]),
-}
-
-
 def normalize(data_numpy):
-    
+
     data_numpy = np.array(data_numpy)
 
     data_numpy[0, :, :, :] = data_numpy[0, :, :, :] - \
         data_numpy[0, :, 0, 0].mean(axis=0)
-    
+
     data_numpy[1, :, :, :] = data_numpy[1, :, :, :] - \
         data_numpy[1, :, 0, 0].mean(axis=0)
 
     return data_numpy
+
+
+def draw_skeleton(img, result, num_keypoint, show_limbs=True):
+    shown_keypoints = joints[num_keypoint]
+    l_pair = np.array(edges[num_keypoint]) - 1
+    part_line = {}
+    kp_preds = result[:, :2]
+    kp_scores = result[:, 2]
+
+    # Draw keypoints
+    for n in range(kp_scores.shape[0]):
+        if kp_scores[n] <= 0.1 or n not in shown_keypoints:
+            continue
+        cor_x, cor_y = int(
+            round(kp_preds[n, 0])), int(round(kp_preds[n, 1]))
+        part_line[n] = (cor_x, cor_y)
+        cv2.circle(img, (cor_x, cor_y), 2, (0, 0, 255), -1)
+
+    if show_limbs:
+        # Draw limbs
+        for start_p, end_p in l_pair:
+            if start_p in part_line and end_p in part_line:
+                start_p = part_line[start_p]
+                end_p = part_line[end_p]
+                cv2.line(img, start_p, end_p, (0, 255, 0), 2)
+    return img
+
+
